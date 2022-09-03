342908aa5c73b66cde7dda615af755fd3a8ec678
kameshX007 <kameshbook@gmail.com>
2022-09-03 17:33:46 +0530

    #

--- initiate_ResetDocker.sh
+++ initiate_ResetDocker.sh
@@ -1,9 +1,6 @@
-#!/bin/sh 
-echo "Starting ResetDocker...">>git.log
-echo "Clonning ResetDocker repo from git...">>git.log
-git clone https://github.com/kameshX007/ResetDocker.git>>git.log
+#!/bin/sh
+git clone https://github.com/kameshX007/ResetDocker.git
 cd ResetDocker
 sh resetDocker.sh
 cd ..
-echo "ResetDocker completed...">>git.log
 exit 0
