7fb4489e7d8344bdcdb79c5f5b1fc6e2a0051527
kameshX007 <kameshbook@gmail.com>
2022-09-06 20:18:02 +0530

    #

--- VScode.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-git clone https://github.com/kameshX007/deployVScode.git
-cd deployVScode
-sh deployVScode.sh $1 $2
-cd ..
-exit 0
---
+++ deployCoadingWS.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+git clone https://github.com/kameshX007/deployCoadingWS.git
+cd deployCoadingWS
+sh deployCoadingWS.sh $1 $2
+cd ..
+exit 0
